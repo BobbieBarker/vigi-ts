b5e0f3ed45c88b5e8a31afc80b775345a3c4a07d
bob <sirkemera@gmail.com>
2015-11-14 20:19:48 -0800

    refactored vigi.ts added baseUrl mutation methods

--- src/main.ts
+++ src/main.ts
@@ -1,72 +1,36 @@
 export let __hotReload = true;
 
-  if (!Object.assign) {
-    Object.defineProperty(Object, 'assign', {
-      enumerable: false,
-      configurable: true,
-      writable: true,
-      value: function(target) {
-        'use strict';
-        if (target === undefined || target === null) {
-          throw new TypeError('Cannot convert first argument to object');
-        }
-  
-        var to = Object(target);
-        for (var i = 1; i < arguments.length; i++) {
-          var nextSource = arguments[i];
-          if (nextSource === undefined || nextSource === null) {
-            continue;
-          }
-          nextSource = Object(nextSource);
-  
-          var keysArray = Object.keys(Object(nextSource));
-          for (var nextIndex = 0, len = keysArray.length; nextIndex < len; nextIndex++) {
-            var nextKey = keysArray[nextIndex];
-            var desc = Object.getOwnPropertyDescriptor(nextSource, nextKey);
-            if (desc !== undefined && desc.enumerable) {
-              to[nextKey] = nextSource[nextKey];
-            }
-          }
-        }
-        return to;
-      }
-    });
-  }
-
-import {_fetch} from './request';
 import {Vigi} from './vigi';
-import {HTTP} from './http';
 
- export const vigi = () => {
-   //let http = new HTTP(_fetch);
-   return new Vigi();
+ export const vigi = (baseUrl: string) => {
+   return new Vigi(baseUrl);
 };
 
-let test = vigi();
+let test = vigi('http://jsonplaceholder.typicode.com');
 
+console.log(test)
 
-test.baseUrl = 'http://jsonplaceholder.typicode.com';
-test.one('posts')
+//test.one('posts')
 
-console.log(test.one('posts'));
+console.log(test)
 
 test.one('posts').get().then(data => {
   console.log(data)
-  data[1].put({title: 'steve', body: 'supa steve?'}).then(data => {
-    console.log('put?', data)
-  })
+  /*data[1].put({title: 'steve', body: 'supa steve?'}).then(data => {
+   // console.log('put?', data)
+  })*/
   data[0].get('comments').then(data => {
     console.log('chained!', data)
   })
 })
 
-test.one('todos').get().then(data => {
+/*test.one('todos').get().then(data => {
   console.log(data)
   console.log(data[0])
   data[0].get().then(data => {
     
   })
-})
+})*/
 
 
 
--- src/vigi.ts
+++ src/vigi.ts
@@ -1,41 +1,72 @@
-export let __hotReload = true;
-import {_fetch} from './request';
-import {HTTP} from './http';
-
-export class Vigi {
-	constructor(){
-		
-	}
-		
-	private _baseUrl: string;
-	
-	get baseUrl(): string {
-		return this._baseUrl
-	}
-	
-	set baseUrl(url: string){
-		this._baseUrl = url;
-	}
-	
-	one(path: string) {
-		let resource = `${this._baseUrl}/${path}`;
-		return new HTTP(resource, _fetch)	
-	}
-	
+interface HttpOptions {
+	METHOD: string
 }
 
+import {_fetch} from './request';
 
+export class Vigi  {
+  private parent: string;
+	public get;
+	public post;
+	public remove;
+	public put;
+  public one;
+  public all;
 
-
-
-
-
-
-
-
-
-
-
-
-
+	constructor(baseUrl: string){
+    this.parent = baseUrl;
+    
+		this.get = (queryParams?: string, HttpOptions?: string) => {
+			return _fetch(this.makeRequest(this, queryParams, 'GET')).then(data => {
+				return this.enhance(data);
+			});
+		}
+		
+		this.post = () => {
+			
+		}
+		
+		this.put = (payload: any, queryParams: string) => {
+			return _fetch(this.makeRequest(this, queryParams, 'PUT', payload)).then(data => {
+				return this.enhance(data);
+			});
+		}
+		
+		this.remove = () => {
+			
+		}
+    
+    this.one = (path: string, id: number) => {
+      this.parent = `${this.parent}/${path}`;
+      this.parent = id ? `${this.parent}/${id}` : this.parent;
+      return this;
+    }
+    
+    this.all = (path: string) => {
+      this.parent = `${this.parent}/${path}`;
+      return this;
+    }
+	}// end constructor
+  
+  //refactor out into function
+	makeRequest(instance: HTTP, params?: string,  method?: string, payload?: any) {
+    
+    let resource = instance.parent;
+    
+		if(instance.hasOwnProperty('id')) {
+			resource = `${resource}/${instance.id}`;
+		}
+    
+		resource = params ? `${resource}/${params}` : resource;
+		return new Request(resource, {method: method, body: JSON.stringify(payload)});
+	}
+	 
+  //refactor out into function
+	enhance(data) {
+		if(Array.isArray(data)){
+			return data.map(item => Object.assign(this, item));
+		}
+		return Object.assign(this, data);
+	}
+}
 
