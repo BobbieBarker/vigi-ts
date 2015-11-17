ad7197d6f955599505e90f772b58d42117087926
Chad King <cking@aerohive.com>
2015-11-16 22:31:01 -0800

    fixed vsc white spacing issues

--- src/request.ts
+++ src/request.ts
@@ -1,5 +1,4 @@
 export let __hotReload = true;
-
 const _fetch = (request: any) => {
   return  window.fetch(request).then((data) => data.json().then((rest) => {
     return rest;
@@ -8,22 +7,22 @@ const _fetch = (request: any) => {
 
 const _createRequest = (instance: any, method: string, params?: string, payload?: any) => {
   let resource = instance.resource;
-
+  
   if(instance.hasOwnProperty('id')) {
     resource = `${resource}/${instance.id}`;
-	}
-
-	resource = params ? `${resource}/${params}` : resource;
+  }
+  
+  resource = params ? `${resource}/${params}` : resource;
   return new Request(resource, {method: method, body: JSON.stringify(payload)});
 }
-	 
+
 const	_enhance = (instance, data) => {
   if(Array.isArray(data)){
     return data.map(item => Object.assign({}, instance, item));
-	}
-	return Object.assign({}, instance, data);
+  }
+  return Object.assign({}, instance, data);
 }
-  
+
 export const _getResource = (instance: any, method: string, queryParams?: string, payload?: any) => {
   return _fetch(_createRequest(instance, method, queryParams, payload)).then(data => {
     return _enhance(instance, data);
--- src/vigi.ts
+++ src/vigi.ts
@@ -8,36 +8,31 @@ export class Vigi {
   public put;
   public one;
   public all;
-
-	constructor(baseUrl: string) {
+  
+  constructor(baseUrl: string) {
     this.resource = baseUrl;
     
-		this.get = function(queryParams?: string) {
+    this.get = function(queryParams?: string) {
       return _getResource(this, 'get', queryParams);
-		}
-		
-		this.post = function(payload: any, queryParams: string) {
-		  return _getResource(this, 'post', queryParams, payload);	
-		}
-
-		this.put = function(queryParams?: string) {
+    }
+    this.post = function(payload: any, queryParams: string) {
+      return _getResource(this, 'post', queryParams, payload);
+    }
+    this.put = function(queryParams?: string) {
       return _getResource(this, 'put', queryParams, this);
-		}
-		
-		this.remove = function(queryParams) {
-			return _getResource(this, 'delete', queryParams);
-		}
-    
+    }
+    this.remove = function(queryParams) {
+      return _getResource(this, 'delete', queryParams);
+    }
     this.one = function(path: string, id: number) {
       this.resource = `${this.resource}/${path}`;
       this.resource = id ? `${this.resource}/${id}` : this.resource;
       return this;
     }
-    
     this.all = function(path: string) {
       this.resource = `${this.resource}/${path}`;
       return this;
     }
-	}// end constructor
+  }// end constructor
 }// end class
 
